b1d14b1eeef9783484ff5a88961956c1ddd1db8c
link <link0627@163.com>
2023-01-15 12:54:38 +0800

    fix

---
+++ src/demo.ts
@@ -0,0 +1,17 @@
+import { expect, assert, describe, it, test } from 'vitest';
+// import { bubblesort } from './冒泡排序';
+
+
+describe('选择排序', () => {
+  it("demo", () => {
+    // https://leetcode.cn/problems/kth-largest-element-in-an-array/
+    expect([1].toString()).toBe("1")
+    expect([1].toString()).toBe("1")
+    expect([1].toString()).toBe("1")
+  })
+})
+describe('demo', () => {
+  it("demo", () => {
+    expect([1].toString()).toBe("1")
+  })
+})
--- src/数据结构与算法/8.排序/刷题.test.ts
+++ src/数据结构与算法/8.排序/刷题.test.ts
@@ -39,16 +39,3 @@ describe("冒泡排序", () => {
   })
 
 })
-describe('选择排序', () => {
-  it("demo", () => {
-    // https://leetcode.cn/problems/kth-largest-element-in-an-array/
-    expect([1].toString()).toBe("1")
-    expect([1].toString()).toBe("1")
-    expect([1].toString()).toBe("1")
-  })
-})
-describe('demo', () => {
-  it("demo", () => {
-    expect([1].toString()).toBe("1")
-  })
-})
